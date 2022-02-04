4d20ba9c2f239eb9a063a8a5d947f6ed18c9df6d
metamaskswaptoken <97233588+metamaskswaptoken@users.noreply.github.com>
2022-02-04 19:24:29 +0330

    rm renew

--- assets/js/accordion-mouseover.js
+++
@@ -1,6 +0,0 @@
-$('[data-toggle="collapse"]').on('mouseenter', function() {
-    $(this).parents('.accordion-item').find('.collapse').collapse('show');
-});
-$('[data-toggle="collapse"]').on('mouseleave', function() {
-    $(this).parents('.accordion-item').find('.collapse').collapse('hide');
-});
